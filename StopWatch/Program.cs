29c216dd54c7dc62a9c9f87e6c69f00bebfc3892
lucasffarias95 <lucasffarias95@gmail.com>
2026-03-04 11:30:33 -0300

    Commit inicial: Estrutura básica do Stopwatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            start();
+        }
+        static void start()
+        {
+            int time = 10;
+            int currentTime = 0;
+            while ( currentTime != time)
+            {
+                currentTime++;
+            }
+        }
+    }
+}
