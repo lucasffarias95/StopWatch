0375c87bef2caaaf25a2d55fdf14a949f8296916
lucasffarias95 <lucasffarias95@gmail.com>
2026-03-04 11:50:42 -0300

    Adicionando Thread sleep para contagem e função do menu

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -4,16 +4,33 @@
     {
         static void Main(string[] args)
         {
-            start();
+            Menu();
         }
-        static void start()
+
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minutos");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+        }
+        static void Start(int time)
+        {
+            
             int currentTime = 0;
+
             while ( currentTime != time)
             {
+                Console.Clear();
                 currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!!");
+            Thread.Sleep(2000);
         }
     }
 }
