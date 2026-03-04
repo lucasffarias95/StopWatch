4c5a9c2ada7f406a14a897f6fa3e6dce4299e446
lucasffarias95 <lucasffarias95@gmail.com>
2026-03-04 12:07:16 -0300

    Alteração na função para dividir a string e iniciar o contador corretamente

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -14,6 +14,13 @@
             Console.WriteLine("M = Minutos");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1,1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            Console.WriteLine(type);
+            Console.WriteLine(time);
         }
         static void Start(int time)
         {
